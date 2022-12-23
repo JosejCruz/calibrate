4573cc6a96fc20fbbe7d0da8fccc2d6910552a06
JosejCruz <jcruz@medicaresoftsrl.com>
2022-12-23 10:34:31 -0400

    commit v1.02

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import Grafica from "./components/Grafica";
+import GraficaPlotly from './components/GraficaPlotly';
 //import './App.css'
 
 function App() {
 
   return (
     <div className="App">
-      <Grafica/>
+      <GraficaPlotly/>
     </div>
   )
 }
