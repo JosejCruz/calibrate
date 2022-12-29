2e05cf5924e1569ae44eeca2f6185ff535b79bf4
JosejCruz <jcruz@medicaresoftsrl.com>
2022-12-29 18:29:20 -0400

    commit v1.07
    muestra graficos

--- src/components/GraficaPlotly.jsx
+++ src/components/GraficaPlotly.jsx
@@ -2,16 +2,39 @@ import React from 'react'
 import Plot from 'react-plotly.js';
 import Data from '../data/Data';
 
-function GraficaPlotly(valores1) {
-  console.log(Data)
-  const dato = Data[0][11]
+function GraficaPlotly(props) {
+  console.log(props.valor1);
+  const valor1 = props.valor1;
+  const arr = Data.map(elemento => Object.entries(elemento));
+  console.log(arr);
+  let datos = arr.filter(item => item[0][0]=== '' + valor1)
+  datos = datos[0][0]
+  console.log(datos);
+
+  //----//----//----//
+
+  console.log(props.valor2);
+  const valor2 = props.valor2;
+  const arr2 = Data.map(elemento => Object.entries(elemento));
+  console.log(arr2);
+  let datos2 = arr2.filter(item => item[0][0]=== '' + valor2)
+  datos2 = datos2[0][0]
+  console.log(datos2);
+
   const data = [
     {
-      x: dato.DataX,
-      y: dato.DataY,
+      x: datos[1].DataX,
+      y: datos[1].DataY,
       type: 'scatter',
       mode: 'lines',
       marker: {color: 'red'},
+    },
+    {
+      x: datos2[1].DataX,
+      y: datos2[1].DataY,
+      type: 'scatter',
+      mode: 'lines',
+      marker: {color: 'blue'},
     },
   ]
 
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,20 +2,59 @@ import React, { useState } from "react";
 import GraficaPlotly from "./GraficaPlotly";
 
 function Menu() {
-  const [valores1, setValores1] = useState(0);
+  const [valor1, setValor1] = useState(11);
+  const [valor2, setValor2] = useState(11)
 
   const HandleValores = (e) => {
-    setValores1(parseInt(e.target.value));
-    console.log(valores1);
+    setValor1(parseInt(e.target.value));
+    console.log(valor1);
   };
+  const HandleValores2 = (e) =>{
+    setValor2(parseInt(e.target.value));
+    console.log(valor2)
+  }
   return (
     <div>
-      <GraficaPlotly datos={valores1} />
+      <GraficaPlotly valor1={valor1} valor2={valor2} />
       <hr />
-      <label htmlFor="Valores">GAMMA No. ${valores1}</label>
+      <label htmlFor="Valores">GAMMA No. ${valor1}</label>
       <hr />
       <input type="color" value="#ff0000" disabled={true}/>
       <select onChange={HandleValores} name="gamma1" id="gamma1">
+        <option value="11">11</option>
+        <option value="12">12</option>
+        <option value="13">13</option>
+        <option value="14">14</option>
+        <option value="15">15</option>
+        <option value="16">16</option>
+        <option value="17">17</option>
+        <option value="18">18</option>
+        <option value="19">19</option>
+        <option value="21">21</option>
+        <option value="22">22</option>
+        <option value="23">23</option>
+        <option value="24">24</option>
+        <option value="25">25</option>
+        <option value="26">26</option>
+        <option value="27">27</option>
+        <option value="28">28</option>
+        <option value="29">29</option>
+        <option value="30">30</option>
+        <option value="31">31</option>
+        <option value="32">32</option>
+        <option value="36">36</option>
+        <option value="38">38</option>
+        <option value="39">39</option>
+        <option value="41">41</option>
+        <option value="42">42</option>
+        <option value="43">43</option>
+        <option value="44">44</option>
+        <option value="45">45</option>
+        <option value="47">47</option>
+      </select>
+      <hr />
+      <input type="color" value="#0000ff" disabled={true}/>
+      <select onChange={HandleValores2} name="gamma2" id="gamma2">
         <option value="11">11</option>
         <option value="12">12</option>
         <option value="13">13</option>
